0bac599d0dc4fd31ff019b1e92e94382aa700302
hp <bhagatnicky1234@gmail.com>
2025-01-18 10:28:44 +0530

    tab commit

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import { Text, View } from 'react-native'
- 
+import Icon from 'react-native-vector-icons/MaterialIcons';
 const App= ()=>{
     return(
         <View>
+            <Icon name="home" size={30} color="#900" />
             <Text>hello</Text>
         </View>
     )
--- TabNav.js
+++ TabNav.js
@@ -2,24 +2,41 @@ import React from 'react'
 import { Text, View, Button } from 'react-native'
 import {NavigationContainer} from '@react-navigation/native'
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs'
-
+import Ionicons from 'react-native-vector-icons/Ionicons';
 const Tab=createBottomTabNavigator();
 const tab= ()=>{
     return(
         <NavigationContainer>
-            <Tab.Navigator>
-                <Tab.Screen name='Login' component={Login}/>
-                <Tab.Screen name='Home' component={Home}/>
+            <Tab.Navigator 
+            screenOptions={{
+                tabBarActiveTintColor: 'orange',
+                tabBarLabelStyle: {
+                  fontSize: 18,
+                  paddingBottom: 5,
+                },
+              }}>
+                <Tab.Screen name='Home' component={Home}
+                options={{
+                  tabBarIcon:({focused})=>(
+                    <Ionicons name='home' size={28} color={focused? 'orange' : 'black'}/>
+                  )
+                }}
+                />
+                <Tab.Screen name='Setting' component={Setting}
+                options={{
+                  tabBarIcon:({focused})=>(
+                    <Ionicons name='settings' size={28} color={focused? 'orange' : 'black'}/>
+                  )
+                }}
+                />
             </Tab.Navigator>
         </NavigationContainer>
     )
 }
-const Login= (props)=>{
+const Setting= (props)=>{
     return(
         <View style={{flex:1,justifyContent:'center',alignItems:'center'}}>
-            <Text style={{fontSize:20}}>Login page</Text>
-            
-            <Button title='Go to home' onPress={()=>props.navigation.navigate("Home",{name,age})}></Button>
+            <Text style={{fontSize:20}}>Setting page</Text>
         </View>
     )
 }
